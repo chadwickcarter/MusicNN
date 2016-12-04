85fed41c902501e27e0218a4c3cbc88962c6a656
chadwickcarter <chadwickcarter95@gmail.com>
2016-12-03 20:15:13 -0500

    Initial commit

---
+++ MusicNeuralNet/src/MusicNeuralNet.java
@@ -0,0 +1,12 @@
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.nnet.Perceptron;
+
+
+public class MusicNeuralNet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
